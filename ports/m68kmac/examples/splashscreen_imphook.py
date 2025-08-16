b53c85f704d0bf51d7b6aaa0ee6e444b1ba33142
Scott Small <smallsco@gmail.com>
2025-08-15 17:06:24 -0700

    Added splash screen / import hook demo

---
+++ ports/m68kmac/examples/splashscreen_imphook.py
@@ -0,0 +1,202 @@
+"""
+Splash Screen / Import Hook demo
+originally written for MacPython IDE by Just van Rossum
+Ported to Micropython m68k by Scott Small
+
+INSTRUCTIONS FOR USE:
+1. Decompress the "splashscreen_imphook.rsrc.hqx" file with Stuffit Expander.
+2. This will create a "splashscreen_imphook.rsrc" file. Open it with ResEdit.
+3. Copy the PICT resource with ID 499 into the micropython application and save changes.
+4. Increase the memory allocation of the micropython application to 4096kb.
+5. Double click on "splashscreen_imphook.py" to run the demo. Click to exit when the
+   loading bar has reached the end.
+"""
+
+# Imports
+# These are needed to display the splash screen
+# so they have to load before our import hook
+import fontmgr
+import machine
+import mactypes
+import qd
+import sys
+import textedit
+import time
+import toolboxutil
+import uctypes
+import windowmgr
+
+
+# Globals
+_real__import__ = None
+_progress = 0
+
+
+def install_importhook():
+    """
+    Overrides Python's import function with our own
+    """
+    global _real__import__
+    import builtins
+    if _real__import__ is None:
+        _real__import__ = builtins.__import__
+        builtins.__import__ = my__import__
+
+
+def uninstall_importhook():
+    """
+    Restores Python's original import function
+    """
+    global _real__import__
+    import builtins
+    if _real__import__ is not None:
+        builtins.__import__ = _real__import__
+        _real__import__ = None
+
+
+def importing(module):
+    """
+    Updates the progress bar in the splash screen with the name of the
+    module that is getting imported, and advances the bar.
+    
+    Note: this is very manual and only works with exactly 7 imports
+    right now, if you want more/less you will need to do math and update
+    the rect sizes manually
+    """
+    global _progress
+    
+    splash = windowmgr.FrontWindow()
+    
+    qd.SetPort(splash)
+    qd.TextFont(fontmgr.kFontIDGeneva)
+    qd.TextSize(9)
+    rect = mactypes.Rect(270, 85, 286, 413)
+    qd.ForeColor(qd.whiteColor)
+    qd.BackColor(qd.blackColor)
+    if module:
+        text = bytearray(b"Importing: " + module)
+        textedit.TETextBox(text, len(text), rect, 0)
+        if not _progress:
+            qd.FrameRect(mactypes.Rect(286, 85, 294, 413))
+        _progress = _progress + 59
+        qd.PaintRect(mactypes.Rect(287, 86, 293, _progress))
+    qd.ForeColor(qd.blackColor)
+    qd.BackColor(qd.whiteColor)
+    time.sleep(1)  # only for the demo, so you can see progress advancing
+
+
+def my__import__(name, globals=None, locals=None, fromlist=(), level=0):
+    """
+    Replacement for the Python built-in import function. Calls importing()
+    with the name of the module to import, which will update the splash
+    screen, and then calls the built-in import function to actually import
+    the module.
+    """
+    try:
+        return sys.modules[name]
+    except KeyError:
+        try:
+            importing(name)
+        except:
+            try:
+                rv = _real__import__(name, globals, locals, fromlist, level)
+            finally:
+                uninstall_importhook()
+            return rv
+        return _real__import__(name, globals, locals, fromlist, level)
+
+
+def pstr(s):
+    """
+    Converts a Python string into a Pascal string
+    """
+    b = mactypes.Str255()
+    b[0] = len(s)
+    for i in range(len(s)):
+        b[i + 1] = ord(s[i])
+    return uctypes.struct(b, mactypes.ConstStringPtr)
+
+
+def getText():
+    """
+    Gets the text that will be shown in the splash screen
+    """
+    text = b"\r".join([
+        b"Splash Screen / Import Hook Demo",
+        b"originally written for MacPython IDE by Just van Rossum",
+        b"updated for Micropython m68k by Scott Small",
+        b"",
+        b"Built with Python %s" % sys.version
+    ])
+    return bytearray(text)
+
+
+def drawSplash(window):
+    """
+    Draws the contents of the splash screen into the window
+    """
+    qd.SetPort(window)
+
+    # set background of window to black
+    qd.ForeColor(qd.blackColor)
+    qd.PaintRect(window.portRect)
+
+    # draw logo
+    # TODO: load the PICT from a pict or resource file
+    #  currently this demo requires a modified version of the micropython
+    #  executable with a PICT resource at id 499 in order to work correctly
+    #  Also you may need to increase the memory allocation to the micropython
+    #  application depending on the size of the PICT that is used here
+    logo = toolboxutil.GetPicture(499)
+    qd.DrawPicture(logo, mactypes.Rect(-20, -10, 180, 504))
+
+    # draw text
+    qd.TextSize(9)
+    qd.TextFont(fontmgr.kFontIDGeneva)
+    qd.BackColor(qd.blackColor)
+    qd.ForeColor(qd.whiteColor)
+    textRect = mactypes.Rect(195, 10, 270, 487)
+    text = getText()
+    textedit.TETextBox(text, len(text), textRect, textedit.teJustCenter)
+    qd.ForeColor(qd.blackColor)
+    qd.BackColor(qd.whiteColor)
+
+    windowmgr.ValidRect(window.portRect)
+
+
+# Hide the Micropython console window
+machine.HideConsole()
+
+# install the import hook
+install_importhook()
+
+# create and draw the splash screen
+splash = windowmgr.NewWindow(
+    uctypes.struct(0, qd.GrafPort),
+    mactypes.Rect(87, 118, 394, 614),
+    pstr(""),
+    False,
+    windowmgr.plainDBox,
+    uctypes.struct(-1, qd.GrafPort),
+    False,
+    0
+)
+windowmgr.ShowWindow(splash)
+drawSplash(splash)
+
+
+# Here's some imports that we're not actually using
+# (well, except for deskmgr & toolboxevent) that will
+# be picked up by our import hook and trigger the
+# progress bar in the splash screen
+import array
+import deskmgr
+import dialogmgr
+import eventmgr
+import io
+import menumgr
+import toolboxevent
+
+# click to exit (after all imports are done)
+while not toolboxevent.Button():
+    deskmgr.SystemTask()
